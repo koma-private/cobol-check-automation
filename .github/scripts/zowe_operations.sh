9ab33684a80ce806742a1060ab46e1178cbbbda7
koma-private <20257214+koma-private@users.noreply.github.com>
2025-05-23 21:23:40 +0900

    fix: accept self-signed certificates.

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -6,16 +6,16 @@
 LOWERCASE_USERNAME=$(echo "$ZOWE_OPT_USER" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$Z_HOST" --port "$Z_PORT" &>/dev/null; then
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$Z_HOST" --port "$Z_PORT" --ru false &>/dev/null; then
     echo "Directory does not exist. Creating it..."
-    zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck -host "$Z_HOST" --port "$Z_PORT"
+    zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck -host "$Z_HOST" --port "$Z_PORT" --ru false
 else
     echo "Directory already exists."
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.17.jar" --host "$Z_HOST" --port "$Z_PORT"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.17.jar" --host "$Z_HOST" --port "$Z_PORT" --ru false
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$Z_HOST" --port "$Z_PORT"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$Z_HOST" --port "$Z_PORT" --ru false
